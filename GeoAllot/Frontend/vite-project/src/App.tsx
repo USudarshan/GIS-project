971ab8fc814576a0e8a8493afddfc83834041d1b
Rishikesh10234 <rishikesh.sharma@ceinsys.com>
2024-11-18 18:49:11 +0530

    map dummy data integrated in React

--- GeoAllot/Frontend/vite-project/src/App.tsx
+++ GeoAllot/Frontend/vite-project/src/App.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { MapContainer, TileLayer, Polygon, Popup } from "react-leaflet";
+// import { MapContainer, TileLayer, Polygon, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import axios from "axios";
+import Map from './component/map';
 
 interface PlotFeature {
   type: string;
@@ -26,10 +27,7 @@ const App = () => {
   const [mapKey, setMapKey] = useState(0);
   const [plotData, setPlotData] = useState<PlotFeature[]>([]);
 
-  useEffect(() => {
-    // Force re-render the map by changing the key
-    setMapKey((prevKey) => prevKey + 1);
-  }, []);
+
 
   // Fetch plot data from the server
   const getMapCoordinates = async () => {
@@ -42,9 +40,10 @@ const App = () => {
     }
   };
 
-  useEffect(() => {
-    getMapCoordinates();
-  }, []);
+  // useEffect(() => {
+  //   const map = L.map('map').setView([17.430869662, 76.131551228], 12); // Initial center coordinates
+  //   // getMapCoordinates();
+  // }, []);
 
   // Dropdown options
   const options = [
@@ -83,47 +82,8 @@ const App = () => {
       </header>
 
       {/* Map Section */}
-      <div className="flex-grow">
-        <MapContainer
-          key={mapKey}
-          center={[25.746, 82.683]}
-          zoom={15}
-          className="h-full w-full"
-        >
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          />
-          {/* Render polygons from plot data */}
-          {plotData.map((feature, index) => {
-            const { geometry, properties } = feature;
-            // console.log("geometry",geometry);
-            // console.log("properties",properties);
-            if (geometry.type === "MultiPolygon") {
-              return geometry.coordinates.map((coords, polygonIndex) => (
-                <Polygon
-                  key={`${index}-${polygonIndex}`}
-                  positions={coords.map(([longitude, latitude]) => [latitude, longitude])}
-                  color="blue"
-                  weight={3}
-                  opacity={0.6}
-                >
-                  {/* <Popup>
-                    <div>
-                      <h4>{properties.name}</h4>
-                      <p>Area: {properties.area} sq km</p>
-                      <p>Status: {properties.status}</p>
-                      <p>Distance to road: {properties.disroad} meters</p>
-                      <p>Distance to water: {properties.diswater} meters</p>
-                    </div>
-                  </Popup> */}
-                </Polygon>
-              ));
-            }
-            return null;
-          })}
-        </MapContainer>
-      </div>
+      <Map/>
+      
     </div>
   );
 };
